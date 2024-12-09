ab9210b8675df09184dd8e6df395578585675d5b
akhilesh <akhileshpookkuttiyil@gmail.com>
2024-12-09 16:45:31 +0530

    added category Component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,17 @@
 import Banner from "./Banner/Banner";
+import Category from "./Category/Category";
 import Hero from "./Hero/Hero";
+// import Menu from "./Menu/Menu";
+// import Offer from "./Offer/Offer";
 
 const Home = () => {
   return (
     <div className="space-y-10">
       <Hero />
       <Banner />
+      <Category />
+      {/* <Offer />
+      <Menu/> */}
     </div>
   );
 };
